b687d47bebfb827526473de75d03eba95a170b92
Uladzislau Ramanouski <Uladzislau_ramanouski@epam.com>
2022-02-20 16:28:48 +0300

    add changeDetection: ChangeDetectionStrategy.OnPush

--- src/app/features/courses/edit-course/edit-course.component.ts
+++ src/app/features/courses/edit-course/edit-course.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { authorValidator } from 'src/app/shared/validators/author-validator';
 
 @Component({
     selector: 'edit-course',
     styleUrls: ['./edit-course.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './edit-course.component.html',
 })
 export class EditCourseComponent {
